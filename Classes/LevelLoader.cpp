b965eef5541f116c01ee5e542ec02e88a6211e11
mihailogazda <mihailogazda@ef3b8a87-af5d-4932-8a30-f3d7fa5abe8e>
2013-01-20 17:24:47 +0000

    Mistype in equation with fixing scale for Circle primitives.

--- Classes/LevelLoader.cpp
+++ Classes/LevelLoader.cpp
@@ -145,7 +145,7 @@ bool LevelLoader::parseNodeToCocosNode(NODEINFO &info, unsigned int type, unsign
 		info.type = 1;
 		CCSprite *a = CCSprite::create("..\\Resources\\circle.png");
 		float width = a->getContentSize().width;
-		info.scale =  info.radius * 2 - width; //	diametar is 2xradius
+		info.scale =  info.radius * 2 / width; //	diametar is 2xradius
 		a->setColor(ccc3(info.color.r, info.color.g, info.color.b));
 
 		toInsert = a;
