8b9116dd602fe7e2dd16b5f7f2042e53b74ed0f2
mihailogazda <mihailogazda@ef3b8a87-af5d-4932-8a30-f3d7fa5abe8e>
2013-01-21 15:05:06 +0000

    Added shape data project file to resource. I need to parse Polygon in shapes, specified in ShapeHelper.h is example of file.

--- Classes/ShapeHelper.h
+++ Classes/ShapeHelper.h
@@ -8,14 +8,55 @@
 using namespace cocos2d;
 using namespace Json;
 
+/*
+Content example.
+{
+    "rigidBodies": [{
+        "name": "dog.png",
+        "imagePath": "dog.png",
+        "origin": {
+            "x": 0,
+            "y": 0
+        },
+        "polygons": [],
+        "circles": [],
+        "shapes": [{
+            "type": "POLYGON",
+            "vertices": [{
+                "x": 0.6200000047683716,
+                "y": 0.7900000214576721
+            }, {
+                "x": 0.6600000262260437,
+                "y": 0.7900000214576721
+            }, {
+                "x": 0.49000000953674316,
+                "y": 0.010000050067901611
+            }, {
+                "x": 0.6000000238418579,
+                "y": 0.7700000405311584
+            }]
+        }]
+    }],
+    "dynamicObjects": []
+}
+*/
+
 class ShapeHelper
 {
+private:
+
+	const char* resourcePath;
+
 public:
 	
-	ShapeHelper()
+	ShapeHelper(const char* resourcePath)
 	{
-		
+		this->resourcePath = resourcePath;
 	}
+
+	bool hasShapeForItem(char* name);
+	bool createShapeForItem(b2Body* body, char* name);
+
 };
 
 
