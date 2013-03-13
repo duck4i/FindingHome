3ef845b09ffb142b9d8ee849e9ddbbc211049135
mihailogazda <mihailogazda@ef3b8a87-af5d-4932-8a30-f3d7fa5abe8e>
2013-03-13 01:22:08 +0000

    Added real menu items to main-menu.js.

--- Resources/main-menu.js
+++ Resources/main-menu.js
@@ -5,10 +5,44 @@ var director = cc.Director.getInstance();
 var winSize = director.getWinSize();
 var font = "Arial";
 
+//	Actions controller
+var MenuItemActions = {
+	doPlay : function() 
+	{
+		cc.log("TODO: PLAY");
+	},
+	doLoad: function() {
+		cc.log("TODO: LOAD");
+	},
+	doSave: function() {
+		cc.log("TODO: SAVE");
+	},	
+	doCredits : function()
+	{
+		cc.log("TODO: CREDITS");
+	},
+	doExit : function()
+	{
+		cc.log("TODO: EXIT");
+	}
+};
+
+var menuItems = [
+	["Play", MenuItemActions.doPlay],
+	["", null],
+	["Load game", MenuItemActions.doLoad],
+	["Save game", MenuItemActions.doSave],
+	["", null],
+	["Credits", MenuItemActions.doCredits],
+	["", null],
+	["Exit", MenuItemActions.doExit]
+];
 
 //	Main menu layer
 var MainMenuLayer = cc.LayerColor.extend(
 {
+	menu: null,
+	
 	ctor : function()
 	{
 		this._super();
@@ -17,20 +51,43 @@ var MainMenuLayer = cc.LayerColor.extend(
 		this.load();
 	},
 	load : function()
-	{		
-		var title = cc.LabelTTF.create("Finding Home", font, 64);
-		var pos = cc.p(winSize.width / 2.0, winSize.height / 2.0);
-		
+	{
+		var title = cc.LabelTTF.create("Finding Home", font, 64);		
 		title.setColor(cc.c3b(255, 255, 255));
-		title.setPosition(pos);
-		title.setAnchorPoint(cc.p(0.5, 0.5));
-		
-		this.addChild(title);
-		
-
+		title.setPosition(cc.p(winSize.width / 2.0, winSize.height / 2.0));
+		this.addChild(title);							
+							
+		this.createMenuItems();		
 	
-	
+		this.setTouchEnabled(true);
 		this.scheduleUpdate();
+	},
+	createMenuItems: function()
+	{
+		cc.log("Found " + menuItems.length + " items");
+		menu = cc.Menu.create();		
+		
+		for (var i =0; i < menuItems.length; i++)
+		{
+			var item = menuItems[i][0];
+			var action = menuItems[i][1];
+			if (item == "")
+			{			
+				continue;
+			}
+			
+			cc.log("Item " + item);
+			var text = cc.MenuItemFont.create(item, action, MenuItemActions);
+			menu.addChild(text);			
+		}
+		
+		menu.alignItemsVertically();
+		
+		var posx = winSize.width / 2;
+		var posy = winSize.height / 2 - menuItems.length * 20;
+		
+		menu.setPosition(cc.p(posx, posy));
+		this.addChild(menu);
 	},
 	update : function()
 	{
